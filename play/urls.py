d8625b450a2859efcaa420bb1c8bacae02daba39
qqs <qiushuangqian@sina.com>
2020-02-06 15:15:52 +0800

    歌曲播放下载

--- play/urls.py
+++ play/urls.py
@@ -1,1 +1,10 @@
-urlpatterns=[]
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # 歌曲播放
+    path('<int:song_id>.html', views.playView, name='play'),
+    # 歌曲下载
+    path('download/<int:song_id>.html', views.downloadView, name='download')
+]
